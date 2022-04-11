b6d4bf140d73f25e9576301b4c71501b45014580
rem029 <elawrenceponce@gmail.com>
2022-04-11 14:51:39 +0300

    frontend types update

--- packages/frontend/src/types/index.ts
+++ packages/frontend/src/types/index.ts
@@ -46,7 +46,7 @@ export type ReportProgressDetailActivity = ReportProgressSummaryActivity & {
 	name: string;
 	comments: string;
 };
-export interface ReportProgressDetailInterface {
+export interface ReportInterface {
 	inspectionNumber: number;
 	inspectionDate: Date;
 	bldgCode: string;
@@ -59,23 +59,32 @@ export interface ReportProgressDetailInterface {
 	module: number;
 	phaseName: string;
 	classificationName: string;
-	activities: ReportProgressDetailActivity[];
 	isCancelled: number;
 }
+export interface ReportProgressDetailInterface extends ReportInterface {
+	activities: ReportProgressDetailActivity[];
+}
 
-export interface ReportProgressSummaryInterface {
-	inspectionNumber: number;
-	inspectionDate: Date;
-	bldgCode: string;
-	ownerName: string;
-	typeCode: string;
-	constructionMethodName: string;
-	projectCode: string;
-	milestoneCode: string;
-	unit: number;
-	module: number;
-	phaseName: string;
-	classificationName: string;
-	activities: ReportProgressSummaryActivity[];
-	isCancelled: number;
+export interface ReportProgressSummaryInterface extends ReportInterface {
+	activityFoundation: number;
+	activitySuperStructure: number;
+	activityPartitionBlockWorkPlaster: number;
+	activityElectricalFirstFix: number;
+	activityMechanicalFirstFix: number;
+	activityWetAreaProofing: number;
+	activityScreed: number;
+	activityFlooringTerrazzoEpoxy: number;
+	activityWallCladding: number;
+	activityElectricalSecondFix: number;
+	activityMechanicalSecondFix: number;
+	activityRoofWaterProofing: number;
+	activityExternalPaint: number;
+	activityInternalPaint: number;
+	activityWindows: number;
+	activityDoors: number;
+	activityHandlRails: number;
+	activityMechanical: number;
+	activityElectrical: number;
+	activityKitchen: number;
+	activityOthers: number;
 }
