4c13971f1575b239d54218b9f66f6e1cdd90f998
Rem Ponce <elawrenceponce@gmail.com>
2022-06-28 11:05:30 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 echo "Update packages"
-yarn bootstrap
+yarn run bootstrap
 
 echo "Build backend"
-yarn build:backend
+yarnn run build:backend
 
 echo "Reloading PM2 Application"
 pm2 reload pm-app-staging
